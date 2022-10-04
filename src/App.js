71738cb4598a914f6b4741a35109bb22a1653d08
fysal <fysal005@gmail.com>
2022-10-04 10:50:40 +0300

    Deleted training page

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Nav from './nav/Nav'
 import { Routes, Route} from 'react-router-dom';
 import Home from './pages/Home'
 import Footer from './components/Footer';
-import Training from './pages/Training';
 
 function App() {
   return (
@@ -11,7 +10,6 @@ function App() {
       <Nav />
       <Routes>
         <Route path="/" element={<Home/>}/>
-        <Route path="/training" element={<Training/>} />
       </Routes>
       <Footer />
     </div>
